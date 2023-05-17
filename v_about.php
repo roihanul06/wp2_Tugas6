89b244dc14a8b870671e84a341456d725d337b7a
Roihanul06 <roihanulfa@gmail.com>
2023-05-17 15:13:32 +0700

    TUGAS 6

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+    <li>Nama Depan : Roihanul</li>
+    <li>Nama Belakang : Falakil amin</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jalan raya muchtar gang poncol sawangan kota macet</li>
+</ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+    <li>Kediri</li>
+ </ul>
+
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+    <li>tidur</li>
+    <li>bermain game</li>
+</ul>
+</section>
